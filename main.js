0828a93dccc25ec3bc32b414f698d54235ce2b26
huxc <455908283@qq.com>
2019-08-20 19:21:42 +0800

    Promise

--- main.js
+++ main.js
@@ -1,37 +1,36 @@
-window.jQuery = function(nodeOrSelector){
+window.jQuery = function (nodeOrSelector) {
   let nodes = {}
-  nodes.addClass = function(){}
-  nodes.html = function(){}
+  nodes.addClass = function () { }
+  nodes.html = function () { }
   return nodes
 }
 
-window.jQuery.ajax = function ({ url, method, body, success, fail }) {
-  let request = new XMLHttpRequest()
-  request.open(method, url)  //配置request
-  request.onreadystatechange = () => {
-    if (request.readyState === 4) {
-      console.log('请求响应都完毕了')
-      if (request.status >= 200 && request.status < 300) {
-        console.log('说明请求成功')
-        success.call(undefined, request.responseText)
-      } else if (request.status >= 400) {
-        console.log('说明请求失败')
-        fail.call(undefined, request)
+window.jQuery.ajax = function ({ url, method, body }) {
+  return new Promise(function (resolve, reject) {
+    let request = new XMLHttpRequest()
+    request.open(method, url)  //配置request
+    request.onreadystatechange = () => {
+      if (request.readyState === 4) {
+        console.log('请求响应都完毕了')
+        if (request.status >= 200 && request.status < 300) {
+          console.log('说明请求成功')
+          resolve.call(undefined, request.responseText)
+        } else if (request.status >= 400) {
+          console.log('说明请求失败')
+          reject.call(undefined, request)
+        }
       }
     }
-  }
-  request.send(body)
+    request.send(body)
+  })
 }
 
 myButton.addEventListener('click', (e) => {
   window.jQuery.ajax({
     url: '/xxx',
     method: 'get',
-    success: (x) => {
-      console.log(x)
-    },
-    fail: (x) => {
-      console.log(x)
-    }
-  })
+  }).then(
+    (text) => { console.log(text) },
+    (request) => { console.log(request) }
+  )
 })
